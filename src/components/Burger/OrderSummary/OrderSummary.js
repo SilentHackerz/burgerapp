e84539c8103e44f9a0f49a34d12239a66768b847
ak-zul65 <ak.zul65@gmaill.com>
2017-11-20 15:25:23 +0530

    basic burger app completed(without nav)

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux'
+import Button from '../../UI/Button/Button'
 const orderSummary =(props)=>{
         const ingredientsSummary=Object.keys(props.ingredients)
             .map(igkey =>{
                 return <li key={igkey}>
-                    <span style={{textTransform:'capitalize'}}>{igkey}</span>--
+                    <span style={{textTransform:'capitalize'}}>{igkey}</span> --
                     {props.ingredients[igkey]}
                 </li>
             })
@@ -15,7 +16,10 @@ const orderSummary =(props)=>{
             <ul>
                 {ingredientsSummary}
             </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <p>continue to checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </Aux>
     )
 }
