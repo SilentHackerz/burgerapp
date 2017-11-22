46d1d51ec163ebd84d8fc55f928137a3f9e6c617
ak-zul65 <ak.zul65@gmaill.com>
2017-11-22 15:47:47 +0530

    burger with react basic functionality before changeing the git name

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux'
+import React,{Component} from 'react';
+import Aux from '../../../hoc/Aux/Aux'
 import Button from '../../UI/Button/Button'
-const orderSummary =(props)=>{
-        const ingredientsSummary=Object.keys(props.ingredients)
+
+class OrderSummary extends Component{
+    render(){
+        const ingredientsSummary=Object.keys(this.props.ingredients)
             .map(igkey =>{
                 return <li key={igkey}>
                     <span style={{textTransform:'capitalize'}}>{igkey}</span> --
-                    {props.ingredients[igkey]}
+                    {this.props.ingredients[igkey]}
                 </li>
             })
-    return(
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientsSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>continue to checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    )
+        return(
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientsSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>continue to checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        )
+    }
 }
 
-export default orderSummary
+export default OrderSummary
